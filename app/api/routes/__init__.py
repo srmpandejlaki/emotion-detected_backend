020d0f42f3eb539e370d0ba5b78b10992bc3899b
Mutiara <mutiarapandejlaki27@gmail.com>
2025-05-04 07:18:13 +0800

    update init routes

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -1,8 +1,10 @@
-# app/api/routes/__init__.py
-from app.api.routes import processing_router
+from app.api.routes import data_collection_router, preprocessing_router, processing_router, validation_router
 
 all_routers = [
+    data_collection_router.router,
+    preprocessing_router.router,
     processing_router.router,
+    validation_router.router,
     # tambahkan yang lain nanti di sini
 ]
 
