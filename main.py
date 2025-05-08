7686e3ef9fb985bd7c0b0714f5e27d781970d219
Mutiara <mutiarapandejlaki27@gmail.com>
2025-05-09 07:43:11 +0800

    mengaktifkan CORS

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router as api_router
 
 app = FastAPI()
 
+# Tambahkan middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # asal frontend kamu
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routing API
 app.include_router(api_router)
 
 # perintah untuk jalankan server
